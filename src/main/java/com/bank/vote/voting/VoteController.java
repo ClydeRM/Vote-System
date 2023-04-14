d0dc969335718aea687a8dfec4a34c49037602d6
ClydeRM <clyde.official@icloud.com>
2023-04-14 16:36:34 +0800

    feat(Controller): Implement Vote Controller
    VoteController is responsible for handling voting requests
    and
    responses.
    
    The class has two methods:
    getVoteItemResults()
    retrieves a
    list of vote item results and returns them to the
    client
    voteRecords()
    allows a user to vote on a set of selected vote
    items.
    
    If the vote is
    successful, the method returns an HTTP 200
    response with a "Success!"
    message. Otherwise, it returns an HTTP 400
    response with a "Bad
    Request!" message.
    
    The user's ID is extracted
    from the authentication
    token and passed to the voteRecordService along
    with the selected vote
    items.

---
+++ src/main/java/com/bank/vote/voting/VoteController.java
@@ -0,0 +1,43 @@
+package com.bank.vote.voting;
+
+
+import com.bank.vote.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/vote")
+public class VoteController {
+
+    private final VoteRecordService voteRecordService;
+
+    @Autowired
+    public VoteController(VoteRecordService voteRecordService) {
+        this.voteRecordService = voteRecordService;
+    }
+
+
+    @GetMapping("/voteRecords")
+    public List<VoteRecordResult> getVoteItemResults() {
+        return voteRecordService.getVoteItemResults();
+    }
+
+    @PostMapping("/voteRecords")
+    public ResponseEntity<String> voteRecords(@RequestBody VoteRecordRequest voteRecordRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User userDetails = (User) authentication.getPrincipal();
+        Integer userId = userDetails.getId();
+        boolean isSuccess = voteRecordService.voteRecords(userId, voteRecordRequest.getSelectedItems());
+        if (isSuccess) {
+            return ResponseEntity.ok().body("Success!");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad Request!");
+        }
+    }
+}
