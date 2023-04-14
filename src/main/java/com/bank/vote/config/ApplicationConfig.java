7b9146d54b9b4dd039f105d20605d2fd5a6acdf3
ClydeRM <clyde.official@icloud.com>
2023-04-14 15:58:11 +0800

    feat(Config): Setup ApplicationConfig
    The userDetailsService() method fetches user details from a custom
    implementation of UserRepository based on the email of the user.
    
    The
    authenticationProvider() method creates an instance of
    DaoAuthenticationProvider, which uses the userDetailsService() method
    and passwordEncoder() method to fetch user details and encode the
    password.
    
    The authenticationManager() method creates an instance of
    AuthenticationManager which authenticates the user based on their
    username and password.
    
    Finally, the passwordEncoder() method creates an
    instance of BCryptPasswordEncoder, which is used to encode the password.

---
+++ src/main/java/com/bank/vote/config/ApplicationConfig.java
@@ -0,0 +1,51 @@
+package com.bank.vote.config;
+
+import com.bank.vote.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // Fetch UserDetail by using userRepository
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        // A DAO, Fetch UserDetail and encode password
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        // Fetch User
+        authProvider.setUserDetailsService(userDetailsService());
+        // Password encoder
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        // Authenticate user base on username and password
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
