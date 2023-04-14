d40f38db3979d419eaf276e65636881c97fb3083
ClydeRM <clyde.official@icloud.com>
2023-04-14 16:21:36 +0800

    feat(DTO): Setup Register DTO
    Added a DTO for user registration requests.
    The RegisterRequest class
    contains fields for the user's username, email, and password.
    This DTO
    will be used to handle user registration requests in the application.

---
+++ src/main/java/com/bank/vote/auth/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.bank.vote.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String username;
+    private String email;
+    private String password;
+}
