2b32c249bf64acf320e9192ce38b341613202ddf
ClydeRM <clyde.official@icloud.com>
2023-04-14 16:37:46 +0800

    feat(Controller): Implement Dashbroad Controller
    The DashboardController class, which allows the user to manage vote
    items. It has three endpoints:
    
    GET /api/v1/dashboard/voteItems:
    Retrieves all the available vote items.
    
    POST
    /api/v1/dashboard/voteItems: Creates a new vote item.
    
    DELETE
    /api/v1/dashboard/voteItems/{itemId}: Deletes a vote item by id.
    
    The
    controller relies on the VoteItemService to perform CRUD operations on
    the vote items.

---
+++ src/main/java/com/bank/vote/dashboard/DashboardController.java
@@ -0,0 +1,39 @@
+package com.bank.vote.dashboard;
+
+import com.bank.vote.voteitem.VoteItem;
+import com.bank.vote.voting.VoteRecordRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/dashboard")
+public class DashboardController {
+
+    private final VoteItemService voteItemService;
+
+    @Autowired
+    public DashboardController(VoteItemService voteItemService) {
+        this.voteItemService = voteItemService;
+    }
+
+    @GetMapping("/voteItems")
+    public List<VoteItem> getAllVoteItem() {
+        return voteItemService.getAllVoteItems();
+    }
+
+    @PostMapping("/voteItems")
+    public ResponseEntity<VoteItem> addVoteItem(@RequestBody VoteItem voteItem) {
+        VoteItem createdVoteItem = voteItemService.addVoteItem(voteItem);
+        return ResponseEntity.ok(createdVoteItem);
+    }
+
+    @DeleteMapping("/voteItems/{itemId}")
+    public ResponseEntity<Void> removeVoteItem(@PathVariable Integer itemId) {
+        voteItemService.removeVoteItem(itemId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
