1ff4a2715039b395000de1074598a0d96aa11840
ClydeRM <clyde.official@icloud.com>
2023-04-14 16:15:30 +0800

    feat(JwtAuthFilter): Setup Filter logic
    JwtAuthenticationFilter is responsible for extracting the JWT token from
    the request header
    verifying the token's validity,
    and updating the
    SecurityContextHolder with user authentication details.
    
    If the request
    is for the login endpoint or the token is not present or invalid,
    it
    continues with the filter chain without updating the
    SecurityContextHolder.
    The filter uses the JwtService,
    UserDetailsService, and TokenRepository,
    which are injected via
    constructor-based dependency injection.

---
+++ src/main/java/com/bank/vote/config/JwtAuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.bank.vote.config;
+
+import com.bank.vote.token.TokenRepository;
+import com.bank.vote.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.beans.Transient;
+import java.io.IOException;
+import java.security.Security;
+
+import jakarta.transaction.TransactionScoped;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+    private final TokenRepository tokenRepository;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        // Haven't Login
+        if (request.getServletPath().contains("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        // If not have token, do next filter
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // Extract jwt payload
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        // If user is not login
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Get User details
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            var isTokenValid = tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            // If token is valid (compare(jwt.username, userDetail.username))
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                // Update SecurityContextHolder and pass request to controller layer
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                // Set Detail on request
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                // Update SecurityContextHolder
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        // Do next filter
+        filterChain.doFilter(request, response);
+    }
+}
