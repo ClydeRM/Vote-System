a6446ed428f463cce37f50a0cdff1c94fb637788
ClydeRM <clyde.official@icloud.com>
2023-04-14 15:54:54 +0800

    feat(VoteRecord): Implement VoteRecordRquest
    Add VoteRecordRequest class for encapsulating the list of selected vote
    items.

---
+++ src/main/java/com/bank/vote/voting/VoteRecordRequest.java
@@ -0,0 +1,17 @@
+package com.bank.vote.voting;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteRecordRequest {
+    private List<Integer> selectedItems;
+}
+
