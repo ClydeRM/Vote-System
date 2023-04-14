7bb89455c280ac0c3d0d3ebf140245fc6e8e7d54
ClydeRM <clyde.official@icloud.com>
2023-04-14 16:33:12 +0800

    feat(Service): Implement VoteItem  service logic
    VoteItemService handling vote items.
    
    The VoteItemService is responsible
    for interacting with the VoteItemRepository to perform CRUD operations
    on vote items.
    
    The service provides methods for getting all vote
    items, adding a new vote item, and removing a vote item by its ID.

---
+++ src/main/java/com/bank/vote/dashboard/VoteItemService.java
@@ -0,0 +1,32 @@
+package com.bank.vote.dashboard;
+
+import com.bank.vote.voterecord.VoteRecordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.bank.vote.voteitem.VoteItemRepository;
+import com.bank.vote.voteitem.VoteItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class VoteItemService {
+
+    private final VoteItemRepository voteItemRepository;
+    @Autowired
+    public VoteItemService(VoteItemRepository voteItemRepository ) {
+        this.voteItemRepository = voteItemRepository;
+    }
+
+    public List<VoteItem> getAllVoteItems() {
+        return voteItemRepository.findAll();
+    }
+
+    public VoteItem addVoteItem(VoteItem voteItem) {
+        return voteItemRepository.save(voteItem);
+    }
+
+    public void removeVoteItem(Integer itemId) {
+        voteItemRepository.deleteById(itemId);
+    }
+}
