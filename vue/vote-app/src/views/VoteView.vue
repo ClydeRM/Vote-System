74824b30dc3fbdea51be81c72225958dd1cfefdf
ClydeRM <clyde.official@icloud.com>
2023-04-15 22:40:38 +0800

    feat(VoteView): Setup btn and list and API
    The VotingPage component allows users to view and submit vote records.
    
    The component contains a "Get Vote Records" button to fetch existing
    records from the API,
    an input field to select items to vote for, and a
    "Submit Vote Records" button to submit the selected items to the
    API.
    
    The component uses Vue's reactive feature to keep track of the
    state of the input field and the list of vote items.
    
    The component
    also uses Axios to make API requests, and stores the access token in
    Vuex for authorization.

---
+++ vue/vote-app/src/views/VoteView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <h1>VotingPage</h1>
+    <button @click="getVoteRecords">Get Vote Records</button>
+    <div>
+      <input type="text" v-model="state.selectedItems">
+      <button @click="submitVoteRecords">Submit Vote Records</button>
+    </div>
+    <ul>
+      <li v-for="(item, index) in voteItems" :key="index">ItemId: {{ item.itemId }} | ItemName: {{ item.itemName }} | TotalCount: {{ item.count }}</li>
+    </ul>
+    <div>
+      <router-link to="/">Dashboard</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+import axios from 'axios';
+import { store } from '../store';
+
+export default {
+  name: 'VoteView',
+
+  setup() {
+    const state = reactive({
+      selectedItems: "",
+    });
+    const voteItems = reactive([]);
+
+    const getVoteRecords = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/vote/voteRecords', {
+          headers: {
+            Authorization: `Bearer ${store.state.accessToken}`,
+          },
+        });
+        voteItems.length = 0;
+        voteItems.push(...response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const submitVoteRecords = async () => {
+      try {
+        const response = await axios.post('http://localhost:8080/api/v1/vote/voteRecords', {
+          selectedItems: Array.from(state.selectedItems.split(','), item => parseInt(item)),
+        }, {
+          headers: {
+            Authorization: `Bearer ${store.state.accessToken}`,
+          },
+        });
+
+        await getVoteRecords();
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {
+      state,
+      voteItems,
+      getVoteRecords,
+      submitVoteRecords,
+    };
+  },
+};
+</script>
