b42e21b8acf29f8e68649144f61520c080a373b3
ClydeRM <clyde.official@icloud.com>
2023-04-15 22:36:41 +0800

    feat(RegisterView): Setup btn and list
    Register provides a form for users to register an account.
    The form
    contains input fields for the user's username, email, and password.
    The
    form submission triggers the register() function,
    which sends a POST
    request to the backend server to register the user.
    If the registration
    is successful, the user's access token and refresh token are saved to
    the store using Vuex,
    and the user is redirected to the homepage.
    
    This
    component uses Vue 3 and relies on Vuex, Vue Router, and Axios. The
    program uses RESTful API to communicate with the backend server.

---
+++ vue/vote-app/src/views/RegisterView.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>RegisterPage</h1>
+  <div class="container">
+    <div>
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="state.username" />
+    </div>
+    <div>
+      <label for="email">Email:</label>
+      <input type="text" id="email" v-model="state.email" />
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="state.password" />
+    </div>
+    <div>
+      <button @click="register">註冊</button>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import { store } from "../store";
+import { useRouter } from "vue-router";
+import axios from "axios";
+export default {
+  setup() {
+    const state = reactive({
+      username: "",
+      email: "",
+      password: "",
+    });
+    const router = useRouter();
+
+    const register = async () => {
+      const { username, email, password } = state;
+
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/api/v1/auth/register",
+          {
+            username: username,
+            email: email,
+            password: password,
+          }
+        );
+
+        store.commit("setAccessToken", response.data.access_token);
+        store.commit("setRefreshToken", response.data.refresh_token);
+
+        router.push("/");
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return {
+      state,
+      register,
+    };
+  },
+};
+</script>
