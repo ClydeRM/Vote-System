c286d9e7e0d0aa128f7ec80e643bde6bb3c93185
ClydeRM <clyde.official@icloud.com>
2023-04-15 09:44:30 +0800

    feat(Repository): Use Procedure Method

--- src/main/java/com/bank/vote/voterecord/VoteRecordRepository.java
+++ src/main/java/com/bank/vote/voterecord/VoteRecordRepository.java
@@ -1,8 +1,16 @@
 package com.bank.vote.voterecord;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 
 public interface VoteRecordRepository extends JpaRepository<VoteRecord, Integer> {
-    Integer countByVoteItemId(Integer itemId);
-    VoteRecord findByUserIdAndVoteItemId(Integer userId, Integer voteItemId);
+//    Integer countByVoteItemId(Integer itemId);
+//    VoteRecord findByUserIdAndVoteItemId(Integer userId, Integer voteItemId);
+
+    @Procedure(name = "countbyvoteitemid")
+    Integer countByVoteItemId(@Param("item_id") Integer itemId);
+
+    @Procedure(name = "findbyuseridandvoteitemid")
+    VoteRecord findByUserIdAndVoteItemId(@Param("user_id") Integer userId, @Param("item_id") Integer voteItemId);
 }
