02cdee35a33ab42ddeff63a9f4f4e3d89e5ff08f
ClydeRM <clyde.official@icloud.com>
2023-04-15 10:52:08 +0800

    chore(Repository): Clean import

--- src/main/java/com/bank/vote/user/UserRepository.java
+++ src/main/java/com/bank/vote/user/UserRepository.java
@@ -2,14 +2,10 @@ package com.bank.vote.user;
 
 import java.util.Optional;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
+
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-//      Optional<User> findByEmail(String email);
-    @Procedure(name = "finduserbyemail")
-    Optional<User> findUserByEmail(@Param("email_in") String email);
+    Optional<User> findByEmail(String email);
 }
