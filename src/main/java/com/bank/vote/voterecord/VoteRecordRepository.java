42542a86d2129609fc4642fb0e661bbda971828a
ClydeRM <clyde.official@icloud.com>
2023-04-15 12:12:48 +0800

    feat(Repository): Add deleteByVoteItemId function

--- src/main/java/com/bank/vote/voterecord/VoteRecordRepository.java
+++ src/main/java/com/bank/vote/voterecord/VoteRecordRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 
 public interface VoteRecordRepository extends JpaRepository<VoteRecord, Integer> {
-//    Integer countByVoteItemId(Integer itemId);
-//    VoteRecord findByUserIdAndVoteItemId(Integer userId, Integer voteItemId);
-
     @Procedure(name = "countbyvoteitemid")
     Integer countByVoteItemId(@Param("item_id") Integer itemId);
 
     @Procedure(name = "findbyuseridandvoteitemid")
     VoteRecord findByUserIdAndVoteItemId(@Param("user_id") Integer userId, @Param("item_id") Integer voteItemId);
+
+    @Procedure(name = "deletebyvoteitemid")
+    void deleteByVoteItemId(@Param("item_id") Integer itemId);
+
 }
