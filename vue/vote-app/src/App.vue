bbe92ea1d1d3528030291c55bf95f223b7ed2a30
ClydeRM <clyde.official@icloud.com>
2023-04-15 22:31:59 +0800

    feat(App): Setup template

---
+++ vue/vote-app/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Vote System</h1>
+    <div>
+      <router-link to="/login">Login</router-link>
+    </div>
+    <div>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
