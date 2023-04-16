b46d72d84d4a8a2bd8c5ef54e7ce89eccd87457c
ClydeRM <clyde.official@icloud.com>
2023-04-16 23:05:16 +0800

    chore(Repository): Make code more readable

--- src/main/java/com/bank/vote/user/UserRepository.java
+++ src/main/java/com/bank/vote/user/UserRepository.java
@@ -3,8 +3,6 @@ package com.bank.vote.user;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
 
 
 public interface UserRepository extends JpaRepository<User, Integer> {
