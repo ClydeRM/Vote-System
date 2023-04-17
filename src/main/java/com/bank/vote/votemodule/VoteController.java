0f572030a743bf1b51eb508d7907c79c33efef42
ClydeRM <clyde.official@icloud.com>
2023-04-17 16:41:16 +0800

    chore(Controller): Make code more readable

--- src/main/java/com/bank/vote/votemodule/VoteController.java
+++ src/main/java/com/bank/vote/votemodule/VoteController.java
@@ -4,6 +4,7 @@ package com.bank.vote.votemodule;
 import com.bank.vote.user.User;
 import com.bank.vote.votemodule.DTO.VoteRecordRequest;
 import com.bank.vote.votemodule.DTO.VoteRecordResponse;
+import com.bank.vote.voterecord.VoteRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,16 +30,21 @@ public class VoteController {
 
 
     @GetMapping("/voteRecords")
-    public List<VoteRecordResponse> getVoteItemResults() {
-        return voteRecordService.getVoteItemResults();
+    public ResponseEntity<List<VoteRecordResponse>> getVoteRecords() {
+        List<VoteRecordResponse> voteRecords =  voteRecordService.getVoteItemResults();
+        if (voteRecords.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(voteRecords);
+        }
     }
 
     @PostMapping("/voteRecords")
-    public ResponseEntity<String> voteRecords(@RequestBody VoteRecordRequest voteRecordRequest) {
+    public ResponseEntity<String> pollVoteRecords(@RequestBody VoteRecordRequest voteRecordRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User userDetails = (User) authentication.getPrincipal();
         Integer userId = userDetails.getId();
-        boolean isSuccess = voteRecordService.voteRecords(userId, voteRecordRequest.getSelectedItems());
+        boolean isSuccess = voteRecordService.addVoteRecords(userId, voteRecordRequest.getSelectedItems());
         if (isSuccess) {
             return ResponseEntity.ok().body("Success!");
         } else {
