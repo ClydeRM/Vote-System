32b85cd0aec0161472d37e301bcf964a933a0e7c
ClydeRM <clyde.official@icloud.com>
2023-04-17 11:46:13 +0800

    feat(Router): Add new route

--- vue/vote-app/src/router/index.js
+++ vue/vote-app/src/router/index.js
@@ -6,6 +6,7 @@ import Home from "../views/HomeView";
 import Login from "../views/LoginView";
 import Register from "../views/RegisterView";
 import Voting from "../views/VoteView";
+import Dashboard from "../views/DashboardView";
 
 const routes = [
   {
@@ -24,6 +25,10 @@ const routes = [
     path: "/voting",
     component: Voting,
   },
+  {
+    path: "/dashboard",
+    component: Dashboard,
+  }
 ];
 
 const router = createRouter({
