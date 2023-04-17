8989eef4d4cccab1e5cc0a8533e29e24e04903b5
ClydeRM <clyde.official@icloud.com>
2023-04-17 12:28:32 +0800

    feat(Repository): Use procedure function

--- src/main/java/com/bank/vote/voteitem/VoteItemRepository.java
+++ src/main/java/com/bank/vote/voteitem/VoteItemRepository.java
@@ -1,6 +1,14 @@
 package com.bank.vote.voteitem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface VoteItemRepository extends JpaRepository<VoteItem, Integer> {
+
+    @Procedure(value = "find_vote_item_by_item_name")
+    Optional<VoteItem> findVoteItemByItemName(@Param("ItemName") String ItemName);
+
 }
